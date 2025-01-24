9850f01090df791c49cc75fe792a12f458b65022
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-24 14:57:19 +0700

    auth success commit

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -25,10 +25,22 @@ export default {
     let email = ref('');
     let password = ref('');
 
+    let error = ref('');
+
     let signUp = async () => {
       // console.log(displayName.value, email.value, password.value);
-    
-      
+
+      try{
+
+        let res = await auth.createUserWithEmailAndPassword(email.value, password.value)
+        if(!res){
+          throw new Error('Could not create user');
+        }
+      }
+      catch(err){
+        console.log(err.message);
+        error.value = err.message;
+      }
 
     }
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,7 @@
-
 import firebase from "firebase/app";
 import "firebase/firestore";
-
 import "firebase/auth";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBxYvCkYqeiCqmqDpn2Jdoo_sdimmkBWNk",
@@ -10,16 +9,14 @@ const firebaseConfig = {
     projectId: "vue-blog-lp",
     storageBucket: "vue-blog-lp.firebasestorage.app",
     messagingSenderId: "195025310394",
-    appId: "1:195025310394:web:46a2881257fb7c8ae2a883"
+    appId: "1:195025310394:web:5438ef47399d86d6e2a883"
 };
 
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
-
 let db = firebase.firestore();
-let timestamp = firebase.firestore.FieldValue.serverTimestamp;
 let auth = firebase.auth();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export default { db , timestamp , auth };
-
+export { db, auth , timestamp };
