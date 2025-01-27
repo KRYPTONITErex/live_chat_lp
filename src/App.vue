59458067c5a7847164ef8f6f6578151695581f7b
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-27 13:31:47 +0700

    navbar and logout butt Done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
 
+  <Navbar></Navbar>
+
   <router-view/>
+
   
 </template>
+
+<script>
+
+import Navbar from './components/Navbar'
+export default {
+  components: { Navbar },
+  
+}
+
+</script>
+
+
+
 
 <style>
 #app {
@@ -10,6 +26,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+
 
 
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  
+    <nav>
+        <div>
+            <p>Hi display name</p>
+            <p class="email">Logged in as Email</p>
+        </div>
+        <button @click="logout">Log Out</button>
+    </nav>
+
+</template>
+
+<script>
+import { auth } from '../firebase/config';
+export default {
+    setup() {
+        let logout = () => {
+            auth.signOut()
+            console.log('user logged out')
+            
+        }
+
+        return { logout }
+
+    }
+
+}
+</script>
+
+<style>
+
+
+
+</style>
