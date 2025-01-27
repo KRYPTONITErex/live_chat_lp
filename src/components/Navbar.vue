3596651717bb6e6246f76a525598a7bd5ab40790
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-27 15:09:52 +0700

    getting current user composables Done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   
-    <nav>
+    <nav v-if="user">
         <div>
-            <p>Hi display name</p>
-            <p class="email">Logged in as Email</p>
+            <p>Hi {{ user.displayName }}</p>
+            <p class="email">Logged in as {{ user.email }}</p>
         </div>
         <button @click="logout">Log Out</button>
     </nav>
@@ -11,16 +11,28 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import getUser from '../composables/getUser';
 import { auth } from '../firebase/config';
+
 export default {
     setup() {
-        let logout = () => {
-            auth.signOut()
-            console.log('user logged out')
-            
+        let error = ref(null)
+        let { user } = getUser()
+
+        let logout = async () => {
+            try{
+                auth.signOut()
+                console.log('user logged out')
+                }
+            catch(err){
+                error.value = err.message
+                console.log(error.value)
+
+            }
         }
 
-        return { logout }
+        return { logout , user, error }
 
     }
 
