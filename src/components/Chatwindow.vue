d41faa4ae911f437ad0304bb617a623c1a28989b
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-30 08:02:25 +0700

    change date form with date fns Commit

--- src/components/Chatwindow.vue
+++ src/components/Chatwindow.vue
@@ -1,9 +1,9 @@
 <template>
   
     <div class="chat-window">
-        <div class="messages" v-for="message in messages" :key="message.id">
-            <div class="single">
-                <span class="created-at"> {{ message.createdAt.toDate() }}</span>
+        <div class="messages">
+            <div class="single" v-for="message in formattedMessages" :key="message.id">
+                <span class="created-at"> {{ message.createdAt }}</span>
                 <span class="name"> {{ message.name }} : <span class="message">{{ message.message }}</span> </span>
                 
             </div>
@@ -14,12 +14,21 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { db } from '../firebase/config';
+import { formatDistanceToNow } from 'date-fns';
 export default {
 
     setup() {
         let messages = ref ([]);
+
+        let formattedMessages = computed(() => {
+            return messages.value.map(mssg => {
+                let formatTime = formatDistanceToNow(mssg.createdAt.toDate());
+                return {...mssg, createdAt: formatTime}
+            })
+        })
+    
         
         db.collection('messages').orderBy('createdAt').onSnapshot(
             (snapshot) => {
@@ -36,7 +45,7 @@ export default {
                 
             }
         )
-        return { messages }
+        return { messages, formattedMessages }
     }
 
 }
@@ -68,9 +77,9 @@ export default {
     flex-direction: column;
     align-content: left;
     padding: 10px;
-    margin-left: 20px;
-    border-radius: 0px 8px 0px 8px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+    margin: 10px 20px;
+    /* border-radius: 0px 8px 0px 8px; */
+    box-shadow: 0 1px 0px rgba(0, 0, 0, 0.05);
 }
 
 .created-at {
